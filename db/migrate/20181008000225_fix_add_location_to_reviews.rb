c245a9d0700eadafcc56bf550b960f1382d9777c
Briana Gude <briana.gude@gmail.com>
2018-10-07 20:08:22 -0400

    fixed migration file

---
+++ db/migrate/20181008000225_fix_add_location_to_reviews.rb
@@ -0,0 +1,11 @@
+class FixAddLocationToReviews < ActiveRecord::Migration[5.2]
+  def change
+    connection.execute("PRAGMA defer_foreign_keys = ON")
+    connection.execute("PRAGMA foreign_keys = OFF")
+    change_column :reviews, :address, :text
+    connection.execute("PRAGMA foreign_keys = ON")
+    connection.execute("PRAGMA defer_foreign_keys = OFF")
+
+
+  end
+end
